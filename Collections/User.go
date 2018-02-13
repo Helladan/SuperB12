13e5e363aba08a5ba6dca5f87bbeb131eb75f985
Anthony Godard <anthonygodard@gmail.com>
2018-02-13 01:02:12 +0100

    Add methods to User struct Save : save an user in DB GetAllUser : return all users in DB FindUserById : find an user by ID in DB

--- Collections/User.go
+++ Collections/User.go
@@ -3,6 +3,8 @@ package Collections
 import (
 	"time"
 	"github.com/globalsign/mgo/bson"
+	"errors"
+	"github.com/Helladan/guakaTrack/App"
 )
 
 // User entity
@@ -16,4 +18,29 @@ type User struct {
 	Password       []byte                `json:"password"`                // User password (bcrypt hash)
 	GlobalRank     string                `json:"global_rank"`             // User global rank
 	GroupID        map[bson.ObjectId]int `json:"groupID"`                 // If so, group(s) who user belong to --- group ID => rank in group
+}
+
+// Save user in DB
+func (user User) Save() error {
+	if user.ID != bson.ObjectId("") {
+		return errors.New("this user already exists in db")
+	}
+
+	err := App.Database.Database.C("users").Insert(user)
+
+	return err
+}
+
+// Return all users in DB
+func GetAllUsers() (users []User) {
+	App.Database.Database.C("users").Find(bson.M{}).All(&users)
+
+	return
+}
+
+// Find user by ID in DB
+func FindUserByID(id bson.ObjectId) (user User) {
+	App.Database.Database.C("users").FindId(id).One(&user)
+
+	return
 }
