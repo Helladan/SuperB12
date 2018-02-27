1dda73da7c274384b866fc6287be4b59f2262e69
Anthony Godard <anthonygodard@gmail.com>
2018-02-27 02:32:29 +0100

    move database logins to secret file

--- App/App.go
+++ App/App.go
@@ -1,11 +1,33 @@
 package App
 
-import "github.com/Helladan/guakaTrack/Controllers"
+import (
+	"github.com/Helladan/guakaTrack/Controllers"
+	"io/ioutil"
+	"encoding/json"
+)
 
-const DATABASE_HOST = "localhost"
-const DATABASE_NAME = "GuakaTrack"
+type secret struct {
+	DatabaseHost     string `json:"database_host"`
+	DatabaseName     string `json:"database_name"`
+	DatabaseUsername string `json:"database_username"`
+	DatabasePassword string `json:"database_password"`
+}
+
+// Get database logs
+func getSecret() secret {
+	raw, _ := ioutil.ReadFile("secret.json")
+
+	var secret secret
+	json.Unmarshal(raw, secret)
+
+	return secret
+}
+
+var secretContent = getSecret()
 
 var Database = Controllers.NewDatabase(
-	DATABASE_HOST,
-	DATABASE_NAME,
+	secretContent.DatabaseHost,
+	secretContent.DatabaseName,
+	secretContent.DatabaseUsername,
+	secretContent.DatabasePassword,
 )
